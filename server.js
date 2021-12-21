8e0705b95e1a992cb172ab32f300e2cdb3bd279d
Davi Marques <davimgcb@gmail.com>
2021-12-21 20:26:10 -0300

    configurando o server.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("rodando")
+import express from "express"
+import { resolve } from "path"
+
+const app = express()
+
+app.use('/', express.static(
+  resolve(
+    __dirname,
+    './build'
+  )
+))
+
+app.listen(process.env.PORT || 3000, (err)=>{
+  if (err){
+    console.log(err)
+  }
+  console.log("funcionando")
+})
